7a34a281e6fa2a789c75e718c28f85e3110e58b0
anupant01 <pantaanupa@gmail.com>
2019-05-05 08:32:25 +0545

    body parse, sql, sequilize initilization

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ var express = require('express');
 
 var app = new express();
 
+var bodyParser = require('body-parser');
+
+
+
+app.use(function (req,res,next) {
+
+	// body...
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader('Access-Control-Allow-Headers','content-type')
+	next();
+	
+})
+
+//this will parse jason data in form body that arrives from client browser
+app.use(bodyParser.json());
+
+
+
+app.post('/v1/users',function(req,res,next){
+
+	console.log(req)
+
+
+})
+
+
+//
 app.listen(3001);
 
 console.log('server reunning');
